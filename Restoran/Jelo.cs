d3fa57903f2e1ae6ba05e95ce53a6a8606fb2e18
mihajlolukic12 <mihajlolukic12@gmail.com>
2020-03-02 15:42:44 +0100

    restoranPre02.03

---
+++ Restoran/Jelo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoran
+{
+    class Jelo
+    {
+        string ime;
+        int vreme;
+        int cena;
+
+        public Jelo () { }
+        public Jelo(string ime,int vreme,int cena)
+        {
+            ime = this.ime;
+            vreme = this.vreme;
+            cena = this.cena;
+        }
+
+        public string getIme()
+        {
+            return ime;
+        }
+        public void setIme(string ime)
+        {
+            this.ime = ime;
+        }
+        public int getVreme()
+        {
+            return vreme;
+        }
+        public void setVreme(int vreme)
+        {
+            this.vreme = vreme;
+        }
+        public int getCena()
+        {
+            return cena;
+        }
+        public void setCena(int cena)
+        {
+            this.cena = cena;
+        }
+
+        }
+    }
---
+++ Restoran/admin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restoran
+{
+    public partial class admin : Form
+    {
+        public admin()
+        {
+            InitializeComponent();
+        }
+
+        static DateTime datum;
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime datumlocal;
+            datumlocal = monthCalendar1.SelectionRange.Start;
+            label2.Text = "Novi datum je: " + datumlocal.ToString("dd.MM.yyyy.");
+            datum = datumlocal;
+            
+        }
+
+        private void admin_Load(object sender, EventArgs e)
+        {
+            monthCalendar1.MaxSelectionCount = 1;
+            label3.Text = datum.ToString();
+        }
+    }
+}
